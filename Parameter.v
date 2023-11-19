670a35a6051714ea7a030d8e7032701fa84ca1bd
JJ2820 <106920117+JJ2820@users.noreply.github.com>
2023-11-20 04:48:12 +0530

    Add files via upload

---
+++ Parameter.v
@@ -0,0 +1,30 @@
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.10.2023 10:59:24
+// Design Name: 
+// Module Name: Parameter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`ifndef PARAMETER_H_
+`define PARAMETER_H_
+`define col 16 // 16 bits instruction memory, data memory
+`define row_i 15 // instruction memory, instructions number, this number can be ch
+`define row_d 8 // The number of data in data memory. We only use 8 data. Do not c
+`define filename "50001111_50001212.txt"
+`define simulation_time #160
+`endif
